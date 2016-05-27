604a3259d9265e17e8b7a7abf82a7c068a9cb414
Felipe Batista Coelho <felipe@live.in>
2016-05-27 15:23:33 -0300

    Implementing WhiteRabbit code and tests

--- src/WhiteRabbit.php
+++ src/WhiteRabbit.php
@@ -2,8 +2,13 @@
 
 class WhiteRabbit
 {
-    public function findMedianLetterInFile($filePath)
-    {
+   /**
+   * Return the letter whose occurrences are the median in a given file.
+   * If the letter count is even, the right median element will be taken.
+   * @param $filePath
+   */
+    public function findMedianLetterInFile($filePath){
+
         return array("letter"=>$this->findMedianLetter($this->parseFile($filePath),$occurrences),"count"=>$occurrences);
     }
 
@@ -11,18 +16,38 @@ class WhiteRabbit
      * Parse the input file for letters.
      * @param $filePath
      */
-    private function parseFile ($filePath)
+    private function parseFile($filePath)
     {
-        //TODO implement this!
+        if (!file_exists($filePath)){
+
+          throw new InvalidArgumentException("File doesn't exist.");
+        }
+
+        // 1. Remove non letter characters
+        // 2. Lowercase all characters
+        return strtolower(preg_replace("([^A-Z|a-z])", "", file_get_contents($filePath)));
     }
 
     /**
      * Return the letter whose occurrences are the median.
+     * If the letter count is even, the right median element will be taken.
      * @param $parsedFile
      * @param $occurrences
      */
     private function findMedianLetter($parsedFile, &$occurrences)
     {
-        //TODO implement this!
+        // {char (ASCII) => occurrences}
+        $orderedOccurrencesArray = count_chars($parsedFile, 1);
+
+        asort($orderedOccurrencesArray);
+
+        $medianIndex = floor(count($orderedOccurrencesArray) / 2);
+
+        $medianLetter = array_keys(array_slice($orderedOccurrencesArray, $medianIndex, 1, true))[0];
+
+        $occurrences = $orderedOccurrencesArray[$medianLetter];
+
+        // ASCII to char
+        return chr($medianLetter);
     }
 }
--- test/WhiteRabbitTest.php
+++ test/WhiteRabbitTest.php
@@ -12,8 +12,8 @@ class WhiteRabbitTest extends PHPUnit_Framework_TestCase
     /** @var WhiteRabbit */
     private $whiteRabbit;
 
-    public function setUp()
-    {
+    public function setUp(){
+
         $this->whiteRabbit = new WhiteRabbit();
         parent::setUp();
     }
@@ -23,17 +23,35 @@ class WhiteRabbitTest extends PHPUnit_Framework_TestCase
      * @dataProvider medianProvider
      */
     public function testMedian($expected, $file){
+
         $result = $this->whiteRabbit->findMedianLetterInFile($file);
+
         $this->assertTrue(in_array($result, $expected));
     }
 
+    /**
+     * @dataProvider exceptionProvider
+     */
+    public function testException($file){
+
+      $this->expectException(\InvalidArgumentException::class);
+
+      $result = $this->whiteRabbit->findMedianLetterInFile($file);
+    }
+
     public function medianProvider(){
+
         return array(
             array(array(array("letter" => "m", "count" => 9240), array("letter" => "f", "count" => 9095)), __DIR__ ."/../txt/text1.txt"),
             array(array(array("letter" => "w", "count" => 13333), array("letter" => "m", "count" => 12641)), __DIR__ ."/../txt/text2.txt"),
-            array(array(array("letter" => "w", "count" => 2227), array("letter" => "g", "count" => 2187)), __DIR__ ."/../txt/text3.txt"),
-            array(array(array("letter" => "w", "count" => 3049)), __DIR__ ."/../txt/text4.txt"),
-            array(array(array("letter" => "z", "count" => 858)), __DIR__ ."/../txt/text5.txt")
+            array(array(array("letter" => "c", "count" => 2684), array("letter" => "m", "count" => 2244)), __DIR__ ."/../txt/text3.txt"),
+            array(array(array("letter" => "c", "count" => 3114), array("letter" => "w", "count" => 3049)), __DIR__ ."/../txt/text4.txt"),
+            array(array(array("letter" => "u", "count" => 18705), array("letter" => "f", "count" => 18122)), __DIR__ ."/../txt/text5.txt")
         );
+    }
+
+    public function exceptionProvider(){
+
+        return array(array("test1.txt"));
     }
 }
